f97c0c498e7126f53b03344ab2f8e61df69da5b0
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-09-01 21:27:03 -0300

    feat: inserir intro cinematográfica antes da autenticação

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { AuthGate } from './components/AuthGate';
+import { CinematicIntroGate } from './components/CinematicIntroGate/CinematicIntroGate';
 import { SocialJuridicoExperience } from './components/SocialJuridicoExperience';
 import { hydrateCaseCatalog } from './lib/caseRepository';
 import './index.css';
@@ -11,10 +12,12 @@ async function bootstrap() {
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <AuthGate>
-        <App />
-        <SocialJuridicoExperience />
-      </AuthGate>
+      <CinematicIntroGate>
+        <AuthGate>
+          <App />
+          <SocialJuridicoExperience />
+        </AuthGate>
+      </CinematicIntroGate>
     </StrictMode>,
   );
 }
